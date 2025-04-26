249e66a7c69de5d42a3dfcd00a3fb3d18c33349d
Ali <ali-se@Alis-MacBook-Pro.local>
2025-04-27 00:43:23 +0330

    seccond commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function toggleSidebar() {
+ function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const menuBtn = document.getElementById("menuBtn");
     const icon = menuBtn.querySelector("i");
-  
+
     sidebar.classList.toggle("active");
-  
+
     if (sidebar.classList.contains("active")) {
       icon.classList.remove("fa-bars");
       icon.classList.add("fa-times");
@@ -13,11 +13,11 @@ function toggleSidebar() {
       icon.classList.add("fa-bars");
     }
   }
-  
+
   function toggleTheme() {
     document.body.classList.toggle("dark");
   }
-  
+
   function toggleDropdown(id) {
     const dropdown = document.getElementById(id);
     dropdown.classList.toggle("show");
